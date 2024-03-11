73904a97417288e01bae81f08a56d02f9cf73a03
Russ <russellkohler8@gmail.com>
2024-03-11 14:08:06 -0500

    Part One Complete

---
+++ helpers/sql.js
@@ -0,0 +1,32 @@
+const { BadRequestError } = require("../expressError");
+
+// THIS NEEDS SOME GREAT DOCUMENTATION.
+
+/**
+ * Helper Function for making selective update queries.
+ *
+ * Uses the SET function on an SQL UPDATE.
+ *
+ * @param dataToUpdate {Object} {field1: newVal, field2: newVal, ...}
+ * @param jsToSql {Object} maps js-style data fields to database column names,
+ *   like { firstName: "first_name", age: "age" }
+ *
+ * @returns {Object} {sqlSetCols, dataToUpdate}
+ */
+
+function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  const keys = Object.keys(dataToUpdate);
+  if (keys.length === 0) throw new BadRequestError("No data");
+
+  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+  const cols = keys.map((colName, idx) =>
+      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+  );
+
+  return {
+    setCols: cols.join(", "),
+    values: Object.values(dataToUpdate),
+  };
+}
+
+module.exports = { sqlForPartialUpdate };
